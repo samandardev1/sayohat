6df8bb438676fed7c8f24bb78e434840227bb64a
samandardev1 <samandar20040423@gmail.com>
2024-12-27 13:52:10 +0500

    sayohat

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    mkq = MediaQuery.of(context).size;
+    mq = MediaQuery.of(context).size;
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         initialRoute: '/',
